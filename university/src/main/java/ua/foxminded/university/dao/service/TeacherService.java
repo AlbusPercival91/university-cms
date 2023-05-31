f3cc8be87ce93e1b49e9b2b918d55d529ae28fb5
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-05-31 04:38:47 +0200

    add basic CRUD operations to TeacherService class

--- university/src/main/java/ua/foxminded/university/dao/service/TeacherService.java
+++ university/src/main/java/ua/foxminded/university/dao/service/TeacherService.java
@@ -1,9 +1,13 @@
 package ua.foxminded.university.dao.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ua.foxminded.university.dao.entities.Teacher;
 import ua.foxminded.university.dao.interfaces.TeacherRepository;
 
 @Slf4j
@@ -12,4 +16,38 @@ import ua.foxminded.university.dao.interfaces.TeacherRepository;
 @Transactional
 public class TeacherService {
 	private final TeacherRepository teacherRepository;
+
+	public int createTeacher(Teacher teacher) {
+		Teacher newTeacher = teacherRepository.save(teacher);
+		log.info("Created teacher with id: {}", newTeacher.getId());
+		return newTeacher.getId();
+	}
+
+	public int deleteTeacherById(int teacherId) {
+		Optional<Teacher> optionalTeacher = teacherRepository.findById(teacherId);
+
+		if (optionalTeacher.isPresent()) {
+			teacherRepository.deleteById(teacherId);
+			log.info("Deleted teacher with id: {}", teacherId);
+			return teacherId;
+		} else {
+			log.warn("Teacher with id {} not found", teacherId);
+			throw new NoSuchElementException("Teacher not found");
+		}
+	}
+
+	public Teacher updateTeacherById(int teacherId, Teacher teacher) {
+		Teacher existingTeacher = teacherRepository.findById(teacherId).orElseThrow(() -> {
+			log.warn("Teacher with id {} not found", teacherId);
+			return new NoSuchElementException("Teacher not found");
+		});
+
+		existingTeacher.setFirstName(teacher.getFirstName());
+		existingTeacher.setLastName(teacher.getLastName());
+		return teacherRepository.save(existingTeacher);
+	}
+
+	public List<Teacher> getAllTeachers() {
+		return teacherRepository.findAll();
+	}
 }
