de436559f4f2a82b658bc9da255ebede49dee766
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-05-31 04:25:52 +0200

    add basic CRUD operations to StudentService class

--- university/src/main/java/ua/foxminded/university/dao/service/StudentService.java
+++ university/src/main/java/ua/foxminded/university/dao/service/StudentService.java
@@ -1,9 +1,13 @@
 package ua.foxminded.university.dao.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ua.foxminded.university.dao.entities.Student;
 import ua.foxminded.university.dao.interfaces.StudentRepository;
 
 @Slf4j
@@ -12,4 +16,38 @@ import ua.foxminded.university.dao.interfaces.StudentRepository;
 @Transactional
 public class StudentService {
 	private final StudentRepository studentRepository;
+
+	public int createStudent(Student student) {
+		Student newStudent = studentRepository.save(student);
+		log.info("Created student with id: {}", newStudent.getId());
+		return newStudent.getId();
+	}
+
+	public int deleteStudentById(int studentId) {
+		Optional<Student> optionalStudent = studentRepository.findById(studentId);
+
+		if (optionalStudent.isPresent()) {
+			studentRepository.deleteById(studentId);
+			log.info("Deleted student with id: {}", studentId);
+			return studentId;
+		} else {
+			log.warn("Student with id {} not found", studentId);
+			throw new NoSuchElementException("Student not found");
+		}
+	}
+
+	public Student updateStudentById(int studentId, Student student) {
+		Student existingStudent = studentRepository.findById(studentId).orElseThrow(() -> {
+			log.warn("Student with id {} not found", studentId);
+			return new NoSuchElementException("Student not found");
+		});
+
+		existingStudent.setFirstName(student.getFirstName());
+		existingStudent.setLastName(student.getLastName());
+		return studentRepository.save(existingStudent);
+	}
+
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
 }
