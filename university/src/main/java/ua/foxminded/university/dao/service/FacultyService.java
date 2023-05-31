c4dc6a89d3a626cbd0d49844c02f1fa504b2db2d
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-05-31 19:57:21 +0200

    add basic CRUD functionality to :
    - FacultyService
    - GroupService
    - StaffService

--- university/src/main/java/ua/foxminded/university/dao/service/FacultyService.java
+++ university/src/main/java/ua/foxminded/university/dao/service/FacultyService.java
@@ -1,9 +1,14 @@
 package ua.foxminded.university.dao.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import javax.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ua.foxminded.university.dao.entities.Faculty;
 import ua.foxminded.university.dao.interfaces.FacultyRepository;
 
 @Slf4j
@@ -12,4 +17,36 @@ import ua.foxminded.university.dao.interfaces.FacultyRepository;
 @Transactional
 public class FacultyService {
 	private final FacultyRepository facultyRepository;
+
+	public int createFaculty(Faculty faculty) {
+		Faculty newFaculty = facultyRepository.save(faculty);
+		log.info("Created faculty with id: {}", newFaculty.getId());
+		return newFaculty.getId();
+	}
+
+	public int deleteFacultyById(int facultyId) {
+		Optional<Faculty> optionalFaculty = facultyRepository.findById(facultyId);
+
+		if (optionalFaculty.isPresent()) {
+			facultyRepository.deleteById(facultyId);
+			log.info("Deleted faculty with id: {}", facultyId);
+			return facultyId;
+		} else {
+			log.warn("Faculty with id {} not found", facultyId);
+			throw new NoSuchElementException("Faculty not found");
+		}
+	}
+
+	public Faculty updateFacultyById(int facultyId, Faculty faculty) {
+		Faculty existingFaculty = facultyRepository.findById(facultyId).orElseThrow(() -> {
+			log.warn("Faculty with id {} not found", facultyId);
+			return new NoSuchElementException("Faculty not found");
+		});
+		BeanUtils.copyProperties(faculty, existingFaculty, "id");
+		return facultyRepository.save(existingFaculty);
+	}
+
+	public List<Faculty> getAllFaculties() {
+		return facultyRepository.findAll();
+	}
 }
--- university/src/main/java/ua/foxminded/university/dao/service/GroupService.java
+++ university/src/main/java/ua/foxminded/university/dao/service/GroupService.java
@@ -1,9 +1,14 @@
 package ua.foxminded.university.dao.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import javax.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ua.foxminded.university.dao.entities.Group;
 import ua.foxminded.university.dao.interfaces.GroupRepository;
 
 @Slf4j
@@ -12,4 +17,36 @@ import ua.foxminded.university.dao.interfaces.GroupRepository;
 @Transactional
 public class GroupService {
 	private final GroupRepository groupRepository;
+
+	public int createGroup(Group group) {
+		Group newGroup = groupRepository.save(group);
+		log.info("Created group with id: {}", newGroup.getId());
+		return newGroup.getId();
+	}
+
+	public int deleteGroupById(int groupId) {
+		Optional<Group> optionalGroup = groupRepository.findById(groupId);
+
+		if (optionalGroup.isPresent()) {
+			groupRepository.deleteById(groupId);
+			log.info("Deleted group with id: {}", groupId);
+			return groupId;
+		} else {
+			log.warn("Group with id {} not found", groupId);
+			throw new NoSuchElementException("Group not found");
+		}
+	}
+
+	public Group updateGroupById(int groupId, Group group) {
+		Group existingGroup = groupRepository.findById(groupId).orElseThrow(() -> {
+			log.warn("Group with id {} not found", groupId);
+			return new NoSuchElementException("Group not found");
+		});
+		BeanUtils.copyProperties(group, existingGroup, "id");
+		return groupRepository.save(existingGroup);
+	}
+
+	public List<Group> getAllGroups() {
+		return groupRepository.findAll();
+	}
 }
