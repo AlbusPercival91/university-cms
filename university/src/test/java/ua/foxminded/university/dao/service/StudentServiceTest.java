26972804210d7109d9f1eb182c3ea8c3b8635674
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-06-15 20:45:56 +0200

    create new test class StudentServiceTest

---
+++ university/src/test/java/ua/foxminded/university/dao/service/StudentServiceTest.java
@@ -0,0 +1,21 @@
+package ua.foxminded.university.dao.service;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+		TimeTableService.class, TimeTableBuilder.class }))
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test-container")
+@Sql(scripts = { "/drop_data.sql", "/init_tables.sql",
+		"/insert_test_data.sql", }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class StudentServiceTest {
+
+}
