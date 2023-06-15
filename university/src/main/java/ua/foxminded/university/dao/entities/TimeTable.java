b442bc64ef35988f14fc2b0b53c9e0aa19436d5c
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-06-15 20:36:28 +0200

    remove useless relations between Student and TimeTable:
    -remove in class Student: @ToString.Exclude
    	@OneToMany(mappedBy = "student")
    	private Set<TimeTable> timeTables;
    
    -remove in classTimeTable: @ManyToOne
    	@JoinColumn(name = "student_id")
    	private Student student;
    
    *Student depends from his Group, so courses for Student same as for
    Group where Student belong

--- university/src/main/java/ua/foxminded/university/dao/entities/TimeTable.java
+++ university/src/main/java/ua/foxminded/university/dao/entities/TimeTable.java
@@ -44,10 +44,6 @@ public class TimeTable {
 	private Teacher teacher;
 
 	@ManyToOne
-	@JoinColumn(name = "student_id")
-	private Student student;
-
-	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
 
