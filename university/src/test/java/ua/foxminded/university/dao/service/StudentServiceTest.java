3de45d903a32bce107a58bb8c38576cf9ec4d9fa
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-06-15 21:09:43 +0200

    work with StudentServiceTest:
    - create method
    testDeleteStudentById_WhenIdNotFound_ShouldThrowNoSuchElementException

--- university/src/test/java/ua/foxminded/university/dao/service/StudentServiceTest.java
+++ university/src/test/java/ua/foxminded/university/dao/service/StudentServiceTest.java
@@ -1,8 +1,11 @@
 package ua.foxminded.university.dao.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,5 +38,11 @@ class StudentServiceTest {
 	void testDeleteStudentById_ShouldReturnStudentId(int studentId) {
 		Assertions.assertEquals(studentId, studentRepository.findById(studentId).get().getId());
 		Assertions.assertEquals(studentId, studentService.deleteStudentById(studentId));
+	}
+
+	@Test
+	void testDeleteStudentById_WhenIdNotFound_ShouldThrowNoSuchElementException() {
+		Exception noSuchElementException = assertThrows(Exception.class, () -> studentService.deleteStudentById(4));
+		Assertions.assertEquals("Student not found", noSuchElementException.getMessage());
 	}
 }
