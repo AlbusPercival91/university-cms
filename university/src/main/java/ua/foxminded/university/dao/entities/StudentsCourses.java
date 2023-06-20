ca751ca5b1309b7a0b0eabd5dd7be09c0352ab84
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-06-20 18:15:03 +0200

    create new entity class StudentsCourses
    that is represents Student and Course relation

---
+++ university/src/main/java/ua/foxminded/university/dao/entities/StudentsCourses.java
@@ -0,0 +1,39 @@
+package ua.foxminded.university.dao.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+@Table(name = "students_courses", schema = "university")
+public class StudentsCourses {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "student_id")
+	private int studentId;
+
+	@Column(name = "course_id")
+	private int courseId;
+
+	public StudentsCourses(int studentId, int courseId) {
+		this.studentId = studentId;
+		this.courseId = courseId;
+	}
+}
