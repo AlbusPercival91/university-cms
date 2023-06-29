74f60543a9d8f301d9cdd9d467c7b3c0f5263596
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-06-29 13:39:12 +0200

    create data generation file: V2__InsertData.sql
    - basic data generation in file V2__InsertData.sql
    - edit .gitignore

---
+++ university/src/main/resources/db/migration/V2__InsertData.sql
@@ -0,0 +1,94 @@
+-- Insert sample data for university.admin
+INSERT INTO university.admin (first_name, last_name, active, email, password)
+VALUES ('Admin', 'User', true, 'admin@example.com', 'adminpassword');
+
+-- Insert sample data for university.classroom
+INSERT INTO university.classroom (build_no, room_no, street)
+VALUES
+    (1, 101, 'Main Street'),
+    (2, 202, 'Park Avenue'),
+    (3, 303, 'Elm Street'),
+    (4, 404, 'Oak Avenue'),
+    (5, 505, 'Maple Street');
+
+-- Insert sample data for university.courses
+INSERT INTO university.courses (course_name, course_description)
+VALUES
+    ('Course 1', 'Course 1 description'),
+    ('Course 2', 'Course 2 description'),
+    ('Course 3', 'Course 3 description'),
+    ('Course 4', 'Course 4 description'),
+    ('Course 5', 'Course 5 description'),
+    ('Course 6', 'Course 6 description'),
+    ('Course 7', 'Course 7 description'),
+    ('Course 8', 'Course 8 description');
+
+-- Insert sample data for university.faculties
+INSERT INTO university.faculties (faculty_name)
+VALUES
+    ('Faculty 1'),
+    ('Faculty 2'),
+    ('Faculty 3'),
+    ('Faculty 4'),
+    ('Faculty 5');
+
+-- Insert sample data for university.departments
+INSERT INTO university.departments (name, faculty_id)
+VALUES
+    ('Department 1', 1),
+    ('Department 2', 2),
+    ('Department 3', 3),
+    ('Department 4', 4),
+    ('Department 5', 5),
+    ('Department 6', 1),
+    ('Department 7', 2),
+    ('Department 8', 3),
+    ('Department 9', 4),
+    ('Department 10', 5);
+
+-- Insert sample data for university.groups
+INSERT INTO university.groups (group_name, faculty_id)
+VALUES
+    ('Group 1', 1),
+    ('Group 2', 2),
+    ('Group 3', 3),
+    ('Group 4', 4),
+    ('Group 5', 5),
+    ('Group 6', 1),
+    ('Group 7', 2),
+    ('Group 8', 3),
+    ('Group 9', 4),
+    ('Group 10', 5);
+
+-- Insert sample data for university.staff
+INSERT INTO university.staff (first_name, last_name, active, email, password, position, "function")
+VALUES
+    ('Staff 1', 'User', true, 'staff1@example.com', 'staff1password', 'Position 1', 'Function 1'),
+    ('Staff 2', 'User', true, 'staff2@example.com', 'staff2password', 'Position 2', 'Function 2'),
+    ('Staff 3', 'User', true, 'staff3@example.com', 'staff3password', 'Position 3', 'Function 3'),
+    ('Staff 4', 'User', true, 'staff4@example.com', 'staff4password', 'Position 4', 'Function 4'),
+    ('Staff 5', 'User', true, 'staff5@example.com', 'staff5password', 'Position 5', 'Function 5');
+
+-- Insert sample data for university.students
+INSERT INTO university.students (first_name, last_name, active, email, password, group_id)
+SELECT
+    CONCAT('Student', id),
+    'User',
+    true,
+    CONCAT('student', id, '@example.com'),
+    CONCAT('student', id, 'password'),
+    FLOOR((id - 1) / 10) + 1
+FROM generate_series(1, 100) id;
+
+-- Insert sample data for university.teachers
+INSERT INTO university.teachers (first_name, last_name, active, email, password, course_id, department_id)
+SELECT
+    CONCAT('Teacher', id),
+    'User',
+    true,
+    CONCAT('teacher', id, '@example.com'),
+    CONCAT('teacher', id, 'password'),
+    (id - 1) % 8 + 1, 
+    FLOOR((id - 1) / 4) + 1
+FROM generate_series(1, 20) id;
+
