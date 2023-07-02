3ea0bdc60683749b683d1531fcfe4607e9f26dfc
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-07-02 19:24:22 +0200

    *shift files scripts.js , styles.css to directory resources/static
    *remove web.xml with directory webapp as it is no need
    
    *refactor WelcomeController (works as expected)

--- university/src/main/java/ua/foxminded/university/controller/WelcomeController.java
+++ university/src/main/java/ua/foxminded/university/controller/WelcomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 
-	@GetMapping("/")
-	public String showHomePage() {
-		return "/home";
-	}
+    @GetMapping("/")
+    public String welcome() {
+        return "home";
+    }
 }
