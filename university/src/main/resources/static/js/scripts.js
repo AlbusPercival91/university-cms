e1cd21da8b2c4d8955f9fb678f6eeb3925c21ba6
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-07-02 23:23:30 +0200

    amend thymeleaf pages:
    - in about and contacts pages change header and link open behavior
    - in home page change link open behavior
    - fix bug in js code that is not allowe to open links from home.html

--- university/src/main/resources/static/js/scripts.js
+++ university/src/main/resources/static/js/scripts.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Add fade-in animation to hero section
-  var heroSection = document.querySelector('.hero');
-  heroSection.classList.add('fade-in');
+    // Add fade-in animation to hero section
+    var heroSection = document.querySelector('.hero');
+    heroSection.classList.add('fade-in');
 
-  // Smooth scrolling for anchor links
-  var navLinks = document.querySelectorAll('nav a');
-  navLinks.forEach(function(link) {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      var target = document.querySelector(this.getAttribute('href'));
-      target.scrollIntoView({ behavior: 'smooth' });
+    // Smooth scrolling for anchor links except for links with target="_blank"
+    var navLinks = document.querySelectorAll('nav a:not([target="_blank"])');
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            // Check if the link is pointing to a section within the same page
+            var target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                e.preventDefault();
+                target.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
     });
-  });
 });
+
