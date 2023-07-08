035fbefb82f47f978062d57c627dac75fce3b70d
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-07-08 14:59:18 +0200

    - successful refactor method searchTeachers in TeacherController
    - modify search.html

--- university/src/main/java/ua/foxminded/university/controller/TeacherController.java
+++ university/src/main/java/ua/foxminded/university/controller/TeacherController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ua.foxminded.university.dao.entities.Faculty;
 import ua.foxminded.university.dao.entities.Teacher;
 import ua.foxminded.university.dao.service.TeacherService;
 
@@ -34,12 +35,37 @@ public class TeacherController {
 	}
 
 	@PostMapping("/teachers/search")
-	public String searchTeachers(@RequestParam("searchType") String searchType, String courseName, Model model) {
-		List<Teacher> teachers = teacherService.findTeachersRelatedToCourse(courseName);
+	public String searchTeachers(@RequestParam("searchType") String searchType,
+			@RequestParam(required = false) String courseName, @RequestParam(required = false) String facultyName,
+			@RequestParam(required = false) Integer departmentId, @RequestParam(required = false) Integer facultyId,
+			@RequestParam(required = false) Integer teacherId, @RequestParam(required = false) String name,
+			@RequestParam(required = false) String familyName, Model model) {
+		List<Teacher> teachers;
+
+		switch (searchType) {
+		case "course":
+			teachers = teacherService.findTeachersRelatedToCourse(courseName);
+			break;
+//		case "faculty":
+//			teachers = teacherService.findAllByDepartmentFaculty(facultyName);
+//			break;
+		case "department":
+			teachers = teacherService.findAllByDepartmentIdAndDepartmentFacultyId(departmentId, facultyId);
+			break;
+//		case "teacher":
+//			teachers = teacherService.findTeacherById(teacherId);
+//			break;
+//		case "nameAndFamilyName":
+//			teachers = teacherService.findTeacherByNameAndFamilyName(name, familyName);
+//			break;
+		default:
+			return "error";
+		}
 
 		for (Teacher teacher : teachers) {
 			teacher.getAdditionalCourses();
 		}
+
 		model.addAttribute("teachers", teachers);
 		return "teachers/list";
 	}
