34436f6b9775931bc91428797078f9c5029c7686
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-07-08 12:59:46 +0200

    works under search panel
    - edit search.html
    - edit TeacherController

--- university/src/main/java/ua/foxminded/university/controller/TeacherController.java
+++ university/src/main/java/ua/foxminded/university/controller/TeacherController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import ua.foxminded.university.dao.entities.Teacher;
 import ua.foxminded.university.dao.service.TeacherService;
 
@@ -22,6 +25,18 @@ public class TeacherController {
 	@GetMapping("/teachers/list")
 	public String getAllTeachersList(Model model) {
 		List<Teacher> teachers = teacherService.getAllTeachers();
+
+		for (Teacher teacher : teachers) {
+			teacher.getAdditionalCourses();
+		}
+		model.addAttribute("teachers", teachers);
+		return "teachers/list";
+	}
+
+	@PostMapping("/teachers/search")
+	public String searchTeachers(@RequestParam("searchType") String searchType,
+			@RequestParam(required = false) String courseName, Model model) {
+		List<Teacher> teachers = teacherService.findTeachersRelatedToCourse(courseName);
 
 		for (Teacher teacher : teachers) {
 			teacher.getAdditionalCourses();
