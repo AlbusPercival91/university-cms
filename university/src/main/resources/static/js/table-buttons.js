1b18ba30b65cb66d06564a63aef599e6909abd9e
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-07-12 23:22:00 +0200

    adjust edit and remove buttons style
    - modify edit-teacher-list.html, styles.css, table-button.js

--- university/src/main/resources/static/js/table-buttons.js
+++ university/src/main/resources/static/js/table-buttons.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const teacherRows = document.querySelectorAll('tbody tr');
 
-    teacherRows.forEach(function(row, index) {
-        const editButton = document.getElementById(`editButton-${index}`);
-        const removeButton = document.getElementById(`removeButton-${index}`);
+    teacherRows.forEach(function(row) {
+        const editIcon = row.querySelector('.edit-icon');
+        const removeIcon = row.querySelector('.remove-icon');
 
         row.addEventListener('mouseover', function() {
-            editButton.style.display = 'inline-block';
-            removeButton.style.display = 'inline-block';
+            editIcon.style.display = 'inline-block';
+            removeIcon.style.display = 'inline-block';
         });
 
         row.addEventListener('mouseout', function() {
-            editButton.style.display = 'none';
-            removeButton.style.display = 'none';
+            editIcon.style.display = 'none';
+            removeIcon.style.display = 'none';
         });
     });
 });
