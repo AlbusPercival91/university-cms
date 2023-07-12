4c2757914e8a3e22d5a6d0be3e12e8e9221777f1
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-07-12 16:45:40 +0200

    refactor code related to admin view:
    - rename thymeleaf files and move related to admin methods from /teacher
    directory to /admin directory
    
    - create new thymeleafs in /admin to utilize admin view and options from
    admin-panel

--- university/src/main/java/ua/foxminded/university/controller/AdminController.java
+++ university/src/main/java/ua/foxminded/university/controller/AdminController.java
@@ -1,14 +1,120 @@
 package ua.foxminded.university.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ua.foxminded.university.dao.entities.Course;
+import ua.foxminded.university.dao.entities.Department;
+import ua.foxminded.university.dao.entities.Teacher;
+import ua.foxminded.university.dao.service.CourseService;
+import ua.foxminded.university.dao.service.DepartmentService;
+import ua.foxminded.university.dao.service.TeacherService;
 
 @Controller
 public class AdminController {
 
+	@Autowired
+	private TeacherService teacherService;
+
+	@Autowired
+	private DepartmentService departmentService;
+
+	@Autowired
+	private CourseService courseService;
+
 	@GetMapping("/admin/main")
 	public String adminMain() {
 		return "admin/main";
+	}
+
+	@GetMapping("/admin/teacher-search-admin")
+	public String adminTeacherSearchPanel() {
+		return "admin/teacher-search-admin";
+	}
+
+	@GetMapping("/admin/create-teacher")
+	public String showCreateTeacherForm(Model model) {
+		List<Department> departments = departmentService.getAllDepartments();
+		List<Course> courses = courseService.getAllCourses();
+		model.addAttribute("departments", departments);
+		model.addAttribute("courses", courses);
+		return "admin/create-teacher";
+	}
+
+	@PostMapping("/admin/create-teacher")
+	public String createTeacher(@ModelAttribute("teacher") @Validated Teacher teacher, Model model,
+			BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		if (bindingResult.hasErrors()) {
+			for (FieldError error : bindingResult.getFieldErrors()) {
+				redirectAttributes.addFlashAttribute(error.getField() + "Error", error.getDefaultMessage());
+			}
+			return "redirect:/admin/create-teacher";
+		}
+
+		int createdTeacher = teacherService.createAndAssignTeacherToCourse(teacher);
+
+		if (createdTeacher != teacher.getId()) {
+			redirectAttributes.addFlashAttribute("errorMessage", "Failed to create the teacher");
+		} else {
+			redirectAttributes.addFlashAttribute("successMessage", "Teacher created successfully");
+		}
+		return "redirect:/admin/create-teacher";
+	}
+
+	@GetMapping("/admin/teacher-card")
+	public String openTeacherCard() {
+		return "admin/teacher-card";
+	}
+
+	@GetMapping("/admin/edit-teacher-list")
+	public String getAllTeachersListAsAdmin(Model model) {
+		List<Teacher> teachers = teacherService.getAllTeachers();
+
+		for (Teacher teacher : teachers) {
+			teacher.getAdditionalCourses();
+		}
+		model.addAttribute("teachers", teachers);
+		return "admin/edit-teacher-list";
+	}
+
+	@PostMapping("/admin/teacher-search-admin")
+	public String searchTeachersAsAdmin(@RequestParam("searchType") String searchType,
+			@RequestParam(required = false) String courseName, @RequestParam(required = false) String facultyName,
+			@RequestParam(required = false) Integer departmentId, @RequestParam(required = false) Integer facultyId,
+			@RequestParam(required = false) Integer teacherId, @RequestParam(required = false) String firstName,
+			@RequestParam(required = false) String lastName, Model model) {
+		List<Teacher> teachers;
+
+		if ("course".equals(searchType)) {
+			teachers = teacherService.findTeachersRelatedToCourse(courseName);
+		} else if ("faculty".equals(searchType)) {
+			teachers = teacherService.findAllByFacultyName(facultyName);
+		} else if ("department".equals(searchType)) {
+			teachers = teacherService.findAllByDepartmentIdAndDepartmentFacultyId(departmentId, facultyId);
+		} else if ("teacher".equals(searchType)) {
+			teachers = new ArrayList<>();
+			teachers.add(teacherService.findTeacherById(teacherId));
+		} else if ("firstNameAndLastName".equals(searchType)) {
+			teachers = new ArrayList<>();
+			teachers.add(teacherService.findTeacherByName(firstName, lastName));
+		} else {
+			return "error";
+		}
+
+		teachers.forEach(Teacher::getAdditionalCourses);
+		model.addAttribute("teachers", teachers);
+		return "admin/edit-teacher-list";
 	}
 
 }
--- university/src/main/java/ua/foxminded/university/controller/TeacherController.java
+++ university/src/main/java/ua/foxminded/university/controller/TeacherController.java
@@ -5,15 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import ua.foxminded.university.dao.entities.Course;
 import ua.foxminded.university.dao.entities.Department;
 import ua.foxminded.university.dao.entities.Teacher;
@@ -45,31 +39,6 @@ public class TeacherController {
 		model.addAttribute("departments", departments);
 		model.addAttribute("courses", courses);
 		return "teachers/create-teacher";
-	}
-
-	@PostMapping("/teachers/create-teacher")
-	public String createTeacher(@ModelAttribute("teacher") @Validated Teacher teacher, Model model,
-			BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-		if (bindingResult.hasErrors()) {
-			for (FieldError error : bindingResult.getFieldErrors()) {
-				redirectAttributes.addFlashAttribute(error.getField() + "Error", error.getDefaultMessage());
-			}
-			return "redirect:/teachers/create-teacher";
-		}
-
-		int createdTeacher = teacherService.createAndAssignTeacherToCourse(teacher);
-
-		if (createdTeacher != teacher.getId()) {
-			redirectAttributes.addFlashAttribute("errorMessage", "Failed to create the teacher");
-		} else {
-			redirectAttributes.addFlashAttribute("successMessage", "Teacher created successfully");
-		}
-		return "redirect:/teachers/create-teacher";
-	}
-
-	@GetMapping("/teachers/teacher-card")
-	public String openTeacherCard() {
-		return "teachers/teacher-card";
 	}
 
 	@GetMapping("/teachers/teacher-list")
