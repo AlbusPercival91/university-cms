bcdfe50064d6dbabd403f53eee26a93d72830352
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-07-22 20:43:14 +0200

    create time table using ceateTimeTableForStudentsAtCourse
    - required refactor method

--- university/src/main/java/ua/foxminded/university/controller/AdminTimeTableController.java
+++ university/src/main/java/ua/foxminded/university/controller/AdminTimeTableController.java
@@ -1,8 +1,11 @@
 package ua.foxminded.university.controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,6 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ua.foxminded.university.dao.entities.ClassRoom;
 import ua.foxminded.university.dao.entities.Course;
@@ -48,12 +52,13 @@ public class AdminTimeTableController {
 
 	@PostMapping("/admin/timetable/course-timetable-form")
 	public String ceateTimeTableForStudentsAtCourse(@ModelAttribute("timetable") @Validated TimeTable timetable,
-			BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+			BindingResult bindingResult, RedirectAttributes redirectAttributes,
+			@RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
+			@RequestParam("timeFrom") @DateTimeFormat(pattern = "HH:mm") LocalTime timeFrom,
+			@RequestParam("timeTo") @DateTimeFormat(pattern = "HH:mm") LocalTime timeTo) {
 		try {
-
-			TimeTable createdTimeTable = timeTableService.createTimeTableForStudentsAtCourse(timetable.getDate(),
-					timetable.getTimeFrom(), timetable.getTimeTo(), timetable.getTeacher(), timetable.getCourse(),
-					timetable.getClassRoom());
+			TimeTable createdTimeTable = timeTableService.createTimeTableForStudentsAtCourse(date, timeFrom, timeTo,
+					timetable.getTeacher(), timetable.getCourse(), timetable.getClassRoom());
 
 			if (createdTimeTable.getId() != timetable.getId()) {
 				redirectAttributes.addFlashAttribute("errorMessage", "Failed to create Time Table");
