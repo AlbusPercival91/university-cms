8ed5b2bc568b1094d1e1b81c88eb22ee70f726ad
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-07-26 18:59:35 +0200

    refactor AdminStaffController
    create and adjust edit-staff-list.html

--- university/src/main/java/ua/foxminded/university/controller/AdminStaffController.java
+++ university/src/main/java/ua/foxminded/university/controller/AdminStaffController.java
@@ -2,9 +2,7 @@ package ua.foxminded.university.controller;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import ua.foxminded.university.dao.entities.Staff;
 import ua.foxminded.university.dao.service.StaffService;
 
