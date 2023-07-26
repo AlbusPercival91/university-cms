a2ef4fd8286c15461eb5892c688ec84b052282eb
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-07-26 19:26:07 +0200

    - add method searchStaffAsAdmin in AdminStaffController
    - create file staff-search.html
    
    - modify edit-staff-list.html

--- university/src/main/java/ua/foxminded/university/controller/AdminStaffController.java
+++ university/src/main/java/ua/foxminded/university/controller/AdminStaffController.java
@@ -1,7 +1,9 @@
 package ua.foxminded.university.controller;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ua.foxminded.university.dao.entities.Staff;
 import ua.foxminded.university.dao.service.StaffService;
@@ -42,5 +45,24 @@ public class AdminStaffController {
 			return "redirect:/admin/staff/edit-staff-list";
 		}
 		return "redirect:" + referrer;
+	}
+
+	@GetMapping("/admin/staff/search-result")
+	public String searchStaffAsAdmin(@RequestParam("searchType") String searchType,
+			@RequestParam(required = false) Integer staffId, @RequestParam(required = false) String firstName,
+			@RequestParam(required = false) String lastName, Model model) {
+		List<Staff> staffList;
+
+		if ("staff".equals(searchType)) {
+			Optional<Staff> optionalStaff = staffService.findStaffById(staffId);
+			staffList = optionalStaff.map(Collections::singletonList).orElse(Collections.emptyList());
+		} else if ("firstNameAndLastName".equals(searchType)) {
+			Optional<Staff> optionalStaff = staffService.findStaffByName(firstName, lastName);
+			staffList = optionalStaff.map(Collections::singletonList).orElse(Collections.emptyList());
+		} else {
+			return "error";
+		}
+		model.addAttribute("staff", staffList);
+		return "admin/staff/edit-staff-list";
 	}
 }
