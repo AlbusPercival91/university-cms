df0ab9f35c114301b29420f7f6f42c10f3328e9f
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-08-19 19:51:25 +0200

    works under AdminTeacherControllerTest:
    - create first test method, refactor code

--- university/src/test/java/ua/foxminded/university/controller/admin/AdminTeacherControllerTest.java
+++ university/src/test/java/ua/foxminded/university/controller/admin/AdminTeacherControllerTest.java
@@ -1,12 +1,16 @@
 package ua.foxminded.university.controller.admin;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ua.foxminded.university.dao.service.CourseService;
+import ua.foxminded.university.dao.service.DepartmentService;
 import ua.foxminded.university.dao.service.TeacherService;
 
 @WebMvcTest(AdminTeacherController.class)
@@ -19,7 +23,18 @@ class AdminTeacherControllerTest {
 
 	@MockBean
 	private TeacherService teacherService;
-	
-	
+
+	@MockBean
+	private DepartmentService departmentService;
+
+	@MockBean
+	private CourseService courseService;
+
+	@Test
+	void testShowCreateTeacherForm() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/admin/teacher/create-teacher"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("admin/teacher/create-teacher"));
+	}
 
 }
