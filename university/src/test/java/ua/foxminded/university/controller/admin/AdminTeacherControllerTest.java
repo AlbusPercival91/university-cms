4a1f37b1dbce2641b9cbec7ba70c36a45be70431
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-08-19 20:01:26 +0200

    works under AdminTeacherControllerTest:
    - refactor teacher-card.html, remove useless peace:  <input
    type="hidden" th:name="${_csrf.parameterName}" th:value="${_csrf.token}"
    />

--- university/src/test/java/ua/foxminded/university/controller/admin/AdminTeacherControllerTest.java
+++ university/src/test/java/ua/foxminded/university/controller/admin/AdminTeacherControllerTest.java
@@ -1,5 +1,8 @@
 package ua.foxminded.university.controller.admin;
 
+import static org.mockito.Mockito.when;
+import java.util.Optional;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -9,6 +12,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ua.foxminded.university.dao.entities.Department;
+import ua.foxminded.university.dao.entities.Faculty;
+import ua.foxminded.university.dao.entities.Teacher;
 import ua.foxminded.university.dao.service.CourseService;
 import ua.foxminded.university.dao.service.DepartmentService;
 import ua.foxminded.university.dao.service.TeacherService;
@@ -37,4 +43,40 @@ class AdminTeacherControllerTest {
 				.andExpect(MockMvcResultMatchers.view().name("admin/teacher/create-teacher"));
 	}
 
+	@Test
+	void testCreateTeacher() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/admin/teacher/create-teacher"))
+				.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+				.andExpect(MockMvcResultMatchers.flash().attributeCount(1))
+				.andExpect(MockMvcResultMatchers.flash().attributeExists("successMessage"));
+	}
+
+	@Test
+	void testOpenTeacherCard_WhenTeacherExists() throws Exception {
+		Faculty faculty = new Faculty("Grifindor");
+
+		Department department = new Department("Department A", faculty);
+		Teacher teacher = new Teacher("Madam", "Trix", true, "trix@mail.com", "1234", department);
+		teacher.setId(1);
+
+		when(teacherService.findTeacherById(teacher.getId())).thenReturn(Optional.of(teacher));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/admin/teacher/teacher-card/{teacherId}", teacher.getId()))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("admin/teacher/teacher-card"))
+				.andExpect(MockMvcResultMatchers.model().attributeExists("teacher"))
+				.andExpect(MockMvcResultMatchers.model().attributeExists("courses"))
+				.andExpect(MockMvcResultMatchers.model().attribute("teacher", Matchers.sameInstance(teacher)));
+	}
+
+	@Test
+	void testOpenTeacherCard_WhenTeacherDoesNotExist() throws Exception {
+		int teacherId = 999;
+
+		when(teacherService.findTeacherById(teacherId)).thenReturn(Optional.empty());
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/admin/teacher/teacher-card/{teacherId}", teacherId))
+				.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+				.andExpect(MockMvcResultMatchers.redirectedUrl("/admin/teacher/edit-teacher-list"));
+	}
 }
