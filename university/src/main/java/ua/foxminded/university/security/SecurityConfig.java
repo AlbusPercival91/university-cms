49b0348a6f942f89c2cb51ba7e69aa72c6395683
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-08-23 23:13:58 +0200

    works under SecurityConfig:
    - temporarily save

--- university/src/main/java/ua/foxminded/university/security/SecurityConfig.java
+++ university/src/main/java/ua/foxminded/university/security/SecurityConfig.java
@@ -2,14 +2,17 @@ package ua.foxminded.university.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	private static final String[] staticResources = { "/css/**", "/js/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/css/**", "/js/**").permitAll()
+		http.authorizeRequests().mvcMatchers(staticResources).permitAll()
 				.antMatchers("/", "/about", "/contacts", "/admin/**", "/teachers/**").permitAll().anyRequest()
 				.authenticated().and().formLogin().loginPage("/login").permitAll();
 	}
