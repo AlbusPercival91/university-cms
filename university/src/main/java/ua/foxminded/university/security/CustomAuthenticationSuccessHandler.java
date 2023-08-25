e75d97b35da3226cc19d3259b4c5ed2d1fdd7ac1
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-08-25 19:02:22 +0200

    refactor CustomAuthenticationSuccessHandler
    refactor admin/main.html

--- university/src/main/java/ua/foxminded/university/security/CustomAuthenticationSuccessHandler.java
+++ university/src/main/java/ua/foxminded/university/security/CustomAuthenticationSuccessHandler.java
@@ -14,16 +14,17 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
-		if (authentication.getAuthorities().stream().anyMatch(authority -> authority.getAuthority().equals("ADMIN"))) {
+		if (authentication.getAuthorities().stream()
+				.anyMatch(authority -> authority.getAuthority().equals(UserRole.ADMIN.toString()))) {
 			response.sendRedirect("/admin/main");
 		} else if (authentication.getAuthorities().stream()
-				.anyMatch(authority -> authority.getAuthority().equals("TEACHER"))) {
+				.anyMatch(authority -> authority.getAuthority().equals(UserRole.TEACHER.toString()))) {
 			response.sendRedirect("/teacher/main");
 		} else if (authentication.getAuthorities().stream()
-				.anyMatch(authority -> authority.getAuthority().equals("STUDENT"))) {
+				.anyMatch(authority -> authority.getAuthority().equals(UserRole.STUDENT.toString()))) {
 			response.sendRedirect("/student/main");
 		} else if (authentication.getAuthorities().stream()
-				.anyMatch(authority -> authority.getAuthority().equals("STAFF"))) {
+				.anyMatch(authority -> authority.getAuthority().equals(UserRole.STAFF.toString()))) {
 			response.sendRedirect("/staff/main");
 		} else {
 			response.sendRedirect("/");
