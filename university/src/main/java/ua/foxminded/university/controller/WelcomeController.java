d252c1b9a40b75ecbfd7a66b73c6e834f85ebb09
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-08-27 20:48:32 +0200

    create custom login form:
    - modify SecurityConfig
    - create method login in WelcomeController class

--- university/src/main/java/ua/foxminded/university/controller/WelcomeController.java
+++ university/src/main/java/ua/foxminded/university/controller/WelcomeController.java
@@ -2,6 +2,7 @@ package ua.foxminded.university.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
@@ -19,6 +20,11 @@ public class WelcomeController {
 	@GetMapping("/contacts")
 	public String contacts() {
 		return "contacts";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
 	}
 
 }
--- university/src/main/java/ua/foxminded/university/security/SecurityConfig.java
+++ university/src/main/java/ua/foxminded/university/security/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().mvcMatchers(staticResources).permitAll().antMatchers("/", "/about", "/contacts")
-				.permitAll().anyRequest().authenticated().and().formLogin()
+				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login")
 				.successHandler(customAuthenticationSuccessHandler()).permitAll().and().logout().logoutUrl("/logout")
 				.logoutSuccessUrl("/").invalidateHttpSession(true).deleteCookies("JSESSIONID");
 	}
