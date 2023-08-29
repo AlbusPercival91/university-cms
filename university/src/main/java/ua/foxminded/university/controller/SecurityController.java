6bebd21de5880bb2101cc5f261459650fb357065
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-08-29 23:34:34 +0200

    handle error flash message if login or pwd not correct:
    - add block with error message in /login.html
    - modify SecurityController method login

--- university/src/main/java/ua/foxminded/university/controller/SecurityController.java
+++ university/src/main/java/ua/foxminded/university/controller/SecurityController.java
@@ -5,13 +5,18 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SecurityController {
 
 	@GetMapping("/login")
-	public String login() {
+	public String login(@RequestParam(value = "error", required = false) String error, Model model) {
+		if (error != null) {
+			model.addAttribute("loginError", true);
+		}
 		return "login";
 	}
 
