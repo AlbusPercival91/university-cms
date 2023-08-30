0efb14e3fc80479f25b27b962efed5c8c67c4dc4
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-08-31 00:24:46 +0200

    add @Slf4j in UserDetailsServiceIml and replace sysout with logger

--- university/src/main/java/ua/foxminded/university/security/UserDetailsServiceImpl.java
+++ university/src/main/java/ua/foxminded/university/security/UserDetailsServiceImpl.java
@@ -11,38 +11,39 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 import ua.foxminded.university.dao.entities.User;
 
+@Slf4j
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	@Override
-	public UserDetails loadUserByUsername(String email) {
-		List<String> entityNames = Arrays.asList("Student", "Teacher", "Staff", "Admin");
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        List<String> entityNames = Arrays.asList("Student", "Teacher", "Staff", "Admin");
 
-		for (String entityName : entityNames) {
-			String nativeQuery = String.format("SELECT u FROM %s u WHERE u.email = :email", entityName);
+        for (String entityName : entityNames) {
+            String nativeQuery = String.format("SELECT u FROM %s u WHERE u.email = :email", entityName);
 
-			try {
-				User user = entityManager.createQuery(nativeQuery, User.class).setParameter("email", email)
-						.getSingleResult();
+            try {
+                User user = entityManager.createQuery(nativeQuery, User.class).setParameter("email", email)
+                        .getSingleResult();
+                String role = entityName.toUpperCase();
+                log.info(buildUserDetails(user, role).toString());
+                return buildUserDetails(user, role);
+            } catch (NoResultException ignored) {
+                // Entity not found for this email, continue to the next entity
+            }
+        }
+        throw new UsernameNotFoundException("User not found");
+    }
 
-				String role = entityName.toUpperCase();
-				System.out.println(buildUserDetails(user, role));
-				return buildUserDetails(user, role);
-			} catch (NoResultException ignored) {
-				// Entity not found for this email, continue to the next entity
-			}
-		}
-		throw new UsernameNotFoundException("User not found");
-	}
-
-	private UserDetails buildUserDetails(User user, String role) {
-		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getHashedPassword(),
-				Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role)));
-	}
+    private UserDetails buildUserDetails(User user, String role) {
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getHashedPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role)));
+    }
 
 }
